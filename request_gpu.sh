ae9e154ea67c520cea74795e7ee752512f839671
Hang Liu <asherliu@users.noreply.github.com>
2018-01-29 22:18:40 -0500

    Update request_gpu.sh

--- request_gpu.sh
+++ request_gpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bsub -q gpu  -R "select[gpu_model0=TeslaC2075]" -R rusage[ngpus_excl_p=1] -R rusage[mem=128000] -W 2:00 -Is bash
+bsub -q gpu  -R "select[gpu_model0=TeslaC2075]" -R rusage[mem=128000] -W 2:00 -Is bash
