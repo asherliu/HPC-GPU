89e139258b36af3626cadbe4929d451aef2e24e1
asherliu <asherliu@gwu.edu>
2018-02-12 18:22:32 -0500

    update

--- lec4/bandwidthtest.cu
+++ lec4/bandwidthtest.cu
@@ -7,14 +7,28 @@ int main(int args, char **argv)
     int *a, *a_d;
 
     cudaMalloc((void **)&a_d, sizeof(int)*N);
-
     a = new int[N];
+    
+    int *b;
+    cudaHostAlloc((void **)&b, sizeof(int)*N, cudaHostAllocDefault);
 
     double time = wtime();
+    cudaMemcpy(a_d, b, sizeof(int)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, b, sizeof(int)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, b, sizeof(int)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, b, sizeof(int)*N, cudaMemcpyHostToDevice);
+    time = wtime() - time;
+
+    std::cout<<"Pinned Bandwidth: "<<((N*4*4)>>20)/time<<" MB/s\n";
+
+    time = wtime();
+    cudaMemcpy(a_d, a, sizeof(int)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, a, sizeof(int)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, a, sizeof(int)*N, cudaMemcpyHostToDevice);
     cudaMemcpy(a_d, a, sizeof(int)*N, cudaMemcpyHostToDevice);
     time = wtime() - time;
 
-    std::cout<<"Bandwidth: "<<((N*4)>>20)/time<<" MB/s\n";
+    std::cout<<"Random Bandwidth: "<<((N*4*4)>>20)/time<<" MB/s\n";
 
     return 0;
 }
