ea99180828a7bc41b7a91dc5188960a389f79ad1
Hang Liu <asherliu@users.noreply.github.com>
2019-02-04 19:45:52 -0500

    Update compile.sh

--- lec2/compile.sh
+++ lec2/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-nvcc -arch=sm_20 gpu_property.cu -o gpu_property.bin
+nvcc gpu_property.cu -o gpu_property.bin
 
-nvcc -arch=sm_20 vector_adder.cu -o vector_adder.bin
+nvcc vector_adder.cu -o vector_adder.bin
  
-nvcc -arch=sm_20 vector_adder_with_err_chk.cu -o vector_adder_with_err_chk.bin
+nvcc vector_adder_with_err_chk.cu -o vector_adder_with_err_chk.bin
 
-nvcc -arch=sm_20 vector_adder_with_err.cu -o vector_adder_with_err.bin
+nvcc vector_adder_with_err.cu -o vector_adder_with_err.bin
